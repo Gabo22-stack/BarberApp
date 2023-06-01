fa5753db4c8455011b3b8b104d5903a0568a05e0
Gabo <agtoloza@misena.edu.co>
2023-06-01 09:57:02 -0500

    Correcciones de codigo

--- CapaPresentacion/Usuario/Cita.Designer.cs
+++ CapaPresentacion/Usuario/Cita.Designer.cs
@@ -28,163 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.MonthCa = new System.Windows.Forms.MonthCalendar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton10 = new System.Windows.Forms.RadioButton();
-            this.radioButton9 = new System.Windows.Forms.RadioButton();
-            this.radioButton8 = new System.Windows.Forms.RadioButton();
-            this.radioButton7 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.Op10 = new System.Windows.Forms.RadioButton();
+            this.Op9 = new System.Windows.Forms.RadioButton();
+            this.Op8 = new System.Windows.Forms.RadioButton();
+            this.Op7 = new System.Windows.Forms.RadioButton();
+            this.Op6 = new System.Windows.Forms.RadioButton();
+            this.Op5 = new System.Windows.Forms.RadioButton();
+            this.Op4 = new System.Windows.Forms.RadioButton();
+            this.Op3 = new System.Windows.Forms.RadioButton();
+            this.Op2 = new System.Windows.Forms.RadioButton();
+            this.Op1 = new System.Windows.Forms.RadioButton();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnConfirmar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.LblDinero = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // monthCalendar1
+            // MonthCa
             // 
-            this.monthCalendar1.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.monthCalendar1.Location = new System.Drawing.Point(85, 90);
-            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(15);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
+            this.MonthCa.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MonthCa.Location = new System.Drawing.Point(85, 90);
+            this.MonthCa.Margin = new System.Windows.Forms.Padding(15);
+            this.MonthCa.Name = "MonthCa";
+            this.MonthCa.TabIndex = 0;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton10);
-            this.groupBox1.Controls.Add(this.radioButton9);
-            this.groupBox1.Controls.Add(this.radioButton8);
-            this.groupBox1.Controls.Add(this.radioButton7);
-            this.groupBox1.Controls.Add(this.radioButton6);
-            this.groupBox1.Controls.Add(this.radioButton5);
-            this.groupBox1.Controls.Add(this.radioButton4);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.Op10);
+            this.groupBox1.Controls.Add(this.Op9);
+            this.groupBox1.Controls.Add(this.Op8);
+            this.groupBox1.Controls.Add(this.Op7);
+            this.groupBox1.Controls.Add(this.Op6);
+            this.groupBox1.Controls.Add(this.Op5);
+            this.groupBox1.Controls.Add(this.Op4);
+            this.groupBox1.Controls.Add(this.Op3);
+            this.groupBox1.Controls.Add(this.Op2);
+            this.groupBox1.Controls.Add(this.Op1);
             this.groupBox1.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(85, 270);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 166);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "TimeRIOS";
+            this.groupBox1.Text = "Horarios";
             // 
-            // radioButton10
+            // Op10
             // 
-            this.radioButton10.AutoSize = true;
-            this.radioButton10.Location = new System.Drawing.Point(106, 138);
-            this.radioButton10.Name = "radioButton10";
-            this.radioButton10.Size = new System.Drawing.Size(62, 22);
-            this.radioButton10.TabIndex = 9;
-            this.radioButton10.TabStop = true;
-            this.radioButton10.Text = "17:00";
-            this.radioButton10.UseVisualStyleBackColor = true;
+            this.Op10.AutoSize = true;
+            this.Op10.Location = new System.Drawing.Point(106, 138);
+            this.Op10.Name = "Op10";
+            this.Op10.Size = new System.Drawing.Size(62, 22);
+            this.Op10.TabIndex = 9;
+            this.Op10.TabStop = true;
+            this.Op10.Text = "17:00";
+            this.Op10.UseVisualStyleBackColor = true;
             // 
-            // radioButton9
+            // Op9
             // 
-            this.radioButton9.AutoSize = true;
-            this.radioButton9.Location = new System.Drawing.Point(106, 114);
-            this.radioButton9.Name = "radioButton9";
-            this.radioButton9.Size = new System.Drawing.Size(62, 22);
-            this.radioButton9.TabIndex = 8;
-            this.radioButton9.TabStop = true;
-            this.radioButton9.Text = "16:15";
-            this.radioButton9.UseVisualStyleBackColor = true;
+            this.Op9.AutoSize = true;
+            this.Op9.Location = new System.Drawing.Point(106, 114);
+            this.Op9.Name = "Op9";
+            this.Op9.Size = new System.Drawing.Size(62, 22);
+            this.Op9.TabIndex = 8;
+            this.Op9.TabStop = true;
+            this.Op9.Text = "16:15";
+            this.Op9.UseVisualStyleBackColor = true;
             // 
-            // radioButton8
+            // Op8
             // 
-            this.radioButton8.AutoSize = true;
-            this.radioButton8.Location = new System.Drawing.Point(106, 88);
-            this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(62, 22);
-            this.radioButton8.TabIndex = 7;
-            this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "15:30";
-            this.radioButton8.UseVisualStyleBackColor = true;
+            this.Op8.AutoSize = true;
+            this.Op8.Location = new System.Drawing.Point(106, 88);
+            this.Op8.Name = "Op8";
+            this.Op8.Size = new System.Drawing.Size(62, 22);
+            this.Op8.TabIndex = 7;
+            this.Op8.TabStop = true;
+            this.Op8.Text = "15:30";
+            this.Op8.UseVisualStyleBackColor = true;
             // 
-            // radioButton7
+            // Op7
             // 
-            this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(106, 63);
-            this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(62, 22);
-            this.radioButton7.TabIndex = 6;
-            this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "14:45";
-            this.radioButton7.UseVisualStyleBackColor = true;
+            this.Op7.AutoSize = true;
+            this.Op7.Location = new System.Drawing.Point(106, 63);
+            this.Op7.Name = "Op7";
+            this.Op7.Size = new System.Drawing.Size(62, 22);
+            this.Op7.TabIndex = 6;
+            this.Op7.TabStop = true;
+            this.Op7.Text = "14:45";
+            this.Op7.UseVisualStyleBackColor = true;
             // 
-            // radioButton6
+            // Op6
             // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(106, 35);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(62, 22);
-            this.radioButton6.TabIndex = 5;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "14:00";
-            this.radioButton6.UseVisualStyleBackColor = true;
+            this.Op6.AutoSize = true;
+            this.Op6.Location = new System.Drawing.Point(106, 35);
+            this.Op6.Name = "Op6";
+            this.Op6.Size = new System.Drawing.Size(62, 22);
+            this.Op6.TabIndex = 5;
+            this.Op6.TabStop = true;
+            this.Op6.Text = "14:00";
+            this.Op6.UseVisualStyleBackColor = true;
             // 
-            // radioButton5
+            // Op5
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(14, 138);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(62, 22);
-            this.radioButton5.TabIndex = 4;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "11:15";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.Op5.AutoSize = true;
+            this.Op5.Location = new System.Drawing.Point(14, 138);
+            this.Op5.Name = "Op5";
+            this.Op5.Size = new System.Drawing.Size(62, 22);
+            this.Op5.TabIndex = 4;
+            this.Op5.TabStop = true;
+            this.Op5.Text = "11:15";
+            this.Op5.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // Op4
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(14, 114);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(62, 22);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "10:30";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.Op4.AutoSize = true;
+            this.Op4.Location = new System.Drawing.Point(14, 114);
+            this.Op4.Name = "Op4";
+            this.Op4.Size = new System.Drawing.Size(62, 22);
+            this.Op4.TabIndex = 3;
+            this.Op4.TabStop = true;
+            this.Op4.Text = "10:30";
+            this.Op4.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // Op3
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(14, 88);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(54, 22);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "9:45";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.Op3.AutoSize = true;
+            this.Op3.Location = new System.Drawing.Point(14, 88);
+            this.Op3.Name = "Op3";
+            this.Op3.Size = new System.Drawing.Size(54, 22);
+            this.Op3.TabIndex = 2;
+            this.Op3.TabStop = true;
+            this.Op3.Text = "9:45";
+            this.Op3.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // Op2
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(14, 63);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(54, 22);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "9:00";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.Op2.AutoSize = true;
+            this.Op2.Location = new System.Drawing.Point(14, 63);
+            this.Op2.Name = "Op2";
+            this.Op2.Size = new System.Drawing.Size(54, 22);
+            this.Op2.TabIndex = 1;
+            this.Op2.TabStop = true;
+            this.Op2.Text = "9:00";
+            this.Op2.UseVisualStyleBackColor = true;
             // 
-            // radioButton1
+            // Op1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(14, 35);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(54, 22);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "8:15";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.Op1.AutoSize = true;
+            this.Op1.Location = new System.Drawing.Point(14, 35);
+            this.Op1.Name = "Op1";
+            this.Op1.Size = new System.Drawing.Size(54, 22);
+            this.Op1.TabIndex = 0;
+            this.Op1.TabStop = true;
+            this.Op1.Text = "8:15";
+            this.Op1.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
@@ -229,17 +230,27 @@
             this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // LblDinero
+            // 
+            this.LblDinero.AutoSize = true;
+            this.LblDinero.Location = new System.Drawing.Point(71, 592);
+            this.LblDinero.Name = "LblDinero";
+            this.LblDinero.Size = new System.Drawing.Size(35, 13);
+            this.LblDinero.TabIndex = 24;
+            this.LblDinero.Text = "label2";
+            // 
             // Cita
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(360, 666);
+            this.Controls.Add(this.LblDinero);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnConfirmar);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.MonthCa);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Cita";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -250,26 +261,28 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MonthCalendar MonthCa;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton10;
-        private System.Windows.Forms.RadioButton radioButton9;
-        private System.Windows.Forms.RadioButton radioButton8;
-        private System.Windows.Forms.RadioButton radioButton7;
-        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton Op5;
+        private System.Windows.Forms.RadioButton Op4;
+        private System.Windows.Forms.RadioButton Op3;
+        private System.Windows.Forms.RadioButton Op2;
+        private System.Windows.Forms.RadioButton Op1;
+        private System.Windows.Forms.RadioButton Op10;
+        private System.Windows.Forms.RadioButton Op9;
+        private System.Windows.Forms.RadioButton Op8;
+        private System.Windows.Forms.RadioButton Op7;
+        private System.Windows.Forms.RadioButton Op6;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConfirmar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label LblDinero;
     }
 }
--- CapaPresentacion/Usuario/Cita.cs
+++ CapaPresentacion/Usuario/Cita.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaEntidades;
+using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,10 +18,19 @@ namespace CapaPresentacion
         {
             InitializeComponent();
         }
-
+        CE_Cita OCE_CITA = new CE_Cita();
+        CN_Cita OCN_CITA = new CN_Cita();
+        public int ValorHora = 0;
+        #region Metodos
+        private void FechaActual()
+        {
+            var fechaActual = DateTime.Now;
+            MonthCa.MinDate = fechaActual;
+        }
+        #endregion
         private void Cita_Load(object sender, EventArgs e)
         {
-
+            FechaActual();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -31,10 +42,60 @@ namespace CapaPresentacion
 
         private void btnConfirmar_Click(object sender, EventArgs e)
         {
+            if (Op1.Checked)
+            {
+                ValorHora = 1;
+            }
+            else if (Op2.Checked)
+            {
+                ValorHora = 2;
+            }
+            else if (Op3.Checked)
+            {
+                ValorHora = 3;
+            }
+            else if (Op4.Checked)
+            {
+                ValorHora = 4;
+            }
+            else if (Op5.Checked)
+            {
+                ValorHora = 5;
+            }
+            else if (Op6.Checked)
+            {
+                ValorHora = 6;
+            }
+            else if (Op7.Checked)
+            {
+                ValorHora = 7;
+            }
+            else if (Op8.Checked)
+            {
+                ValorHora = 8;
+            }
+            else if (Op9.Checked)
+            {
+                ValorHora = 9;
+            }
+            else if (Op10.Checked)
+            {
+                ValorHora = 10;
+            }
+            OCE_CITA.Date = MonthCa.SelectionStart; // Obtener el valor de MonthCalendar
+            OCE_CITA.Time = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, ValorHora, 0, 0);// Crear un objeto DateTime con la fecha actual y
+            OCE_CITA.Price = Convert.ToInt32(LblDinero.Text);                                                       // establecer solo la hora
+            OCE_CITA.State = 1;
+            //OCE_CITA.User = 3162206491;
+            //OCE_CITA.Service = Convert.ToInt32();
+            OCE_CITA.Barber = OCE_CITA.Barber;
+            OCE_CITA.Employee = OCE_CITA.Employee;
+            OCN_CITA.CrearCita(OCE_CITA);
             MessageBox.Show("¡Su cita ha sido agendada con exito!");
             Principal newform = new Principal();
             this.Hide();
-            newform.Show();
+            //newform.Show();
         }
+
     }
 }
